74fbe5578befe596cf9133655ae3b6f6a23af56e
HelloIloveGimkit2 <eliaspitre@protonmail.com>
2025-01-07 14:37:16 -0600

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -1,4 +1,4 @@
-local CoreGui = game:GetService("CoreGui")
+local CoreGui = game:GetService("Players").LocalPlayer.PlayerGui
 local UserInput = game:GetService("UserInputService")
 local HttpService = game:GetService("HttpService")
 
